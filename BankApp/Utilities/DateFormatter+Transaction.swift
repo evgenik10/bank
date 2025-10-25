9a94a202632ee63345dc11c75f2633d13e6c7856
evgenik10 <je.klimov@yandex.com>
2025-10-26 02:07:06 +0300

    Delete BankApp directory

--- BankApp/Utilities/DateFormatter+Transaction.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension DateFormatter {
-    static let transaction: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        return formatter
-    }()
-}
